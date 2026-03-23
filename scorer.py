7269d2e0e99e845c680159337b1ff855305ca1c8
CornyPun <nkl.courtney@gmail.com>
2026-03-23 17:17:07 -0600

    Basic Shot Identification

--- scorer.py
+++ scorer.py
@@ -4,8 +4,17 @@ from matplotlib import pyplot as plt
 
 def read_image(path):
     original_img = cv.imread(path)
-    original_img = cv.resize(original_img, (int(original_img.shape[1] * 0.5), int(original_img.shape[0] * 0.5)), interpolation=cv.INTER_AREA)
+    original_img = cv.resize(original_img, (int(original_img.shape[1] * 0.3), int(original_img.shape[0] * 0.3)), interpolation=cv.INTER_AREA)
+    original_img = cv.rotate(original_img, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+    img = cv.cvtColor(original_img, cv.COLOR_BGR2GRAY)
+    img = cv.GaussianBlur(img, (5, 5), 0)
     
+    return original_img, img
+
+def read_frame(frame):
+    original_img = cv.resize(frame, (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.5)), interpolation=cv.INTER_AREA)
+    original_img = frame
     img = cv.cvtColor(original_img, cv.COLOR_BGR2GRAY)
     img = cv.GaussianBlur(img, (5, 5), 0)
     
@@ -16,17 +25,27 @@ def scoresheet_border(img):
 
     ret, thresh = cv.threshold(img, 127, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # cv.drawContours(img, contours, -1, (0, 255, 0), thickness=3)
+    # cv.imshow("e", img)
+    # cv.waitKey(0)
 
     for contour in contours:
-        if largest_contour is None or cv.arcLength(contour,True) > cv.arcLength(largest_contour,True):
+        if largest_contour is None or (cv.arcLength(contour,True) > cv.arcLength(largest_contour,True)):
             largest_contour = contour
-            
-    epsilon = 0.1*cv.arcLength(largest_contour,True)
-    approx = cv.approxPolyDP(largest_contour,epsilon,True)
+    
+    if largest_contour is not None:
+        epsilon = 0.1*cv.arcLength(largest_contour,True)
+        approx = cv.approxPolyDP(largest_contour,epsilon,True)
 
-    return approx
+        # cv.drawContours(img, [approx], 0, (0, 255, 0), 3)
+        # cv.imshow("e", img)
+
+        return approx
+    else:
+        return None
 
 def order_points(pts):
+    print(pts)
     pts = pts.reshape(4, 2)
     rect = np.zeros((4, 2), dtype="float32")
 
@@ -65,7 +84,7 @@ def warped_scoresheet(img, border):
 
     return warped
 
-def find_circles(img):
+def find_targets(img):
     img_blur = cv.medianBlur(img, 21)
 
     rows = img.shape[0]
@@ -77,23 +96,80 @@ def find_circles(img):
             center = (i[0], i[1])
 
             # circle center
-            cv.circle(img, center, 1, (0, 100, 100), 3)
+            #cv.circle(img, center, 1, (0, 100, 100), 3)
 
             # circle outline
             radius = i[2]
-            cv.circle(img, center, radius, (255, 0, 0), 3)
+            #cv.circle(img, center, radius, (255, 0, 0), 3)
+
+            # cropped_img = img[int(i[1] - radius):int(i[1] + radius), int(i[0] - radius):int(i[0] + radius)]
+            # cv.imshow("e", cropped_img)
     else:
         print("No circles found")
 
     return img
 
-original_img, img = read_image("Photos/scoresheet_2.jpg")
-original_img = cv.rotate(original_img, cv.ROTATE_90_COUNTERCLOCKWISE)
-img = cv.rotate(img, cv.ROTATE_90_COUNTERCLOCKWISE)
+def find_shots(img):
+    #cv.imshow("og", img)
+    #img = img[int(img.shape[0] * 1/8):int(img.shape[0] - img.shape[0] * 1/16), :]
+    img = img[:, int(img.shape[1] * 1/12):int(img.shape[1] - img.shape[1] * 1/12)]
+    copy_img = cv.GaussianBlur(img, (5,5), 3)
+    copy_img = cv.cvtColor(copy_img, cv.COLOR_BGRA2GRAY)
+    copy_img = cv.Canny(copy_img, 50, 150)
+    #cv.imshow("edited", copy_img)
 
-border = scoresheet_border(img)
-warped = warped_scoresheet(original_img, border)
+    ret, thresh = cv.threshold(copy_img, 127, 255, 0)
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-cv.imshow("Scoresheet", find_circles(cv.cvtColor(warped, cv.COLOR_BGR2GRAY)))
+    for contour in contours:
+        (x,y),radius = cv.minEnclosingCircle(contour)
+        center = (int(x),int(y))
+        radius = int(radius)
+        
+        if 4 <= radius <= 5 and 2 * np.pi * radius - cv.arcLength(contour, True) < 10:
+            cv.circle(img, center, radius, (0, 255, 0), 1)
+        #elif radius >= 15:
+            #cv.drawContours(copy_img, [contour], 0, (0, 0, 0), 1)
 
-cv.waitKey(0)
+    return copy_img, img
+
+source = cv.VideoCapture(1)
+source.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+source.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+
+win_name = "Camera Preview"
+cv.namedWindow(win_name, cv.WINDOW_NORMAL)
+
+while not(cv.waitKey(1) & 0xFF == ord('q')):
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+    original_img, img = read_frame(frame)
+
+    border = scoresheet_border(img)
+    print(border)
+    if border is not None:
+        if len(border) == 4:
+            warped = warped_scoresheet(original_img, border)
+            scoresheet = find_targets(cv.cvtColor(warped, cv.COLOR_BGR2GRAY))
+            scoresheet = cv.cvtColor(scoresheet, cv.COLOR_GRAY2BGR)
+            copy_img, img = find_shots(scoresheet)
+            cv.imshow(win_name + "-2", copy_img)
+            cv.imshow(win_name, img)
+        else:
+            cv.drawContours(frame, [border], 0, (0, 255, 0), 3)
+            cv.imshow(win_name, frame)
+
+source.release()
+cv.destroyAllWindows()
+
+# original_img, img = read_image("Photos/scoresheet_12.jpg")
+# border = scoresheet_border(img)
+
+# warped = warped_scoresheet(original_img, border)
+# scoresheet = find_targets(cv.cvtColor(warped, cv.COLOR_BGR2GRAY))
+# scoresheet = cv.cvtColor(scoresheet, cv.COLOR_GRAY2BGR)
+# copy_img, img = find_shots(scoresheet)
+# cv.imshow("Scoresheet", img)
+
+# cv.waitKey(0)
